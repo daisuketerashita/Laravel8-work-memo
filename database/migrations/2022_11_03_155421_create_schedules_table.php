a862de764a377129b2eceef0cb94b42c9a30ca5c
寺下大典 <ekusiad.tera@icloud.com>
2022-11-03 16:05:23 +0900

    Scheduleコントローラーとモデルの作成

---
+++ database/migrations/2022_11_03_155421_create_schedules_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSchedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('schedules', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade')->onDelete('cascade');
+            $table->date('sch_date');
+            $table->string('sch_part');
+            $table->bigInteger('exercise_id');
+            $table->text('sch_contents');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('schedules');
+    }
+}
