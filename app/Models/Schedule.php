5027491937b48042ba54becca44aa47bd15d108a
寺下大典 <ekusiad.tera@icloud.com>
2022-11-06 20:19:19 +0900

    種目登録用のモデルとコントローラの作成

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Schedule extends Model
 {
     use HasFactory;
+
+    //リレーション
+    public function exercises()
+    {
+        return $this->hasMany('App\Models\Exercise');
+    }
 }
